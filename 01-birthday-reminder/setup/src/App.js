59a40f6b269727e95b89f1b9f2254d019ccf40b4
olehhladkov <oleh.hladkov@gmail.com>
2021-04-25 17:28:52 +0300

    Create birthday reminder

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
-function App() {
-  return <h2>reminder project setup</h2>;
+import React from 'react'
+
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map(({ id, name, age, image }) => {
+        return (
+          <article className="person" key={id}>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        )
+      })}
+    </>
+  )
 }
 
-export default App;
+export default List
