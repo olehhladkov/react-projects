294f6a98d353cec6c2a1beb86145213f3b432ffe
olehhladkov <oleh.hladkov@gmail.com>
2021-04-25 17:32:07 +0300

    Add menu project

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,10 +1,42 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from 'react'
+import Menu from './Menu'
+import Categories from './Categories'
+import items from './data'
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items)
+
+  const CATEGORY_ALL = 'all'
+  const categories = [
+    CATEGORY_ALL,
+    ...new Set(items.map(({ category }) => category)),
+  ]
+
+  const changeCategory = (category) => {
+    if (category === CATEGORY_ALL) {
+      return setMenuItems(items)
+    }
+
+    const newMenuItems = items.filter(
+      (menuItem) => menuItem.category === category
+    )
+
+    setMenuItems(newMenuItems)
+  }
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+
+        <Categories categories={categories} changeCategory={changeCategory} />
+        <Menu menuItems={menuItems} />
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from 'react'
 
-const Categories = () => {
-  return <h2>categories component</h2>;
-};
+const Categories = ({ categories, changeCategory }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={category}
+            onClick={() => changeCategory(category)}
+          >
+            {category}
+          </button>
+        )
+      })}
+    </div>
+  )
+}
 
-export default Categories;
+export default Categories
